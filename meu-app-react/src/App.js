00541fbe4c62495d360735ed49b5d5434406c3e3
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-02-25 16:45:33 -0300

    first commit

---
+++ meu-app-react/src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import Foto from './img/3g1a6125-768x572-1625074056-1625139726.jpg'
+
+
+
+export default function App(){
+  return(
+    <>
+      
+       <center>
+      <p>GERALDO RUFINO - EMPREENDEDOR</p>
+      
+      <img src={Foto}/>
+      </center>
+
+     
+    </>
+  )
+}
+
+
---
+++ meu-app-react/src/img/Rufino.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import photoRufino from './3g1a6125-768x572-1625074056-1625139726.jpg'
+
+
+export default function Rufino(){
+    return(
+        <header>
+            <img src={photoRufino}/>
+            <h1>Geraldo Rufino</h1>
+
+        </header>
+    )
+
+
+
+
+
+
+
+}
